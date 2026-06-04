133192010b071de33fdeefc3d391ff08b137c99c
Anne-Hélène <ahbricet@gmail.com>
2026-06-04 17:12:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Confetti from "react-confetti";
 import "./index.css";
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
   const gameWon =
     dice.every((die) => die.isHeld) &&
     dice.every((die) => die.value === dice[0].value);
